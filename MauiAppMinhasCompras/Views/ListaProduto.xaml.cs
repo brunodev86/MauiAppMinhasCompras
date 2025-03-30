807d758fc2633200a7cc36b4fd32285f0fd98aa1
bruno rosendo alves <brunora86@gmail.com>
2025-03-30 13:20:46 -0300

    interface gráfica necessária para preencher os dados e salvar no SQLite.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,9 +1,31 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+		lst_produtos.ItemsSource = lista;
 	}
+
+    protected async override void OnAppearing()
+    {
+		List<Produto> tmp = await App.Db.GetAll();
+		tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try 
+		{
+			Navigation.PushAsync(new Views.NovoProduto());
+		} catch (Exception ex) 
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
