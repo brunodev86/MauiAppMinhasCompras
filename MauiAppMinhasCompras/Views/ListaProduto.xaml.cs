11c5102ba5dfd701e562637f8195647536de0025
bruno rosendo alves <brunora86@gmail.com>
2025-03-30 18:02:59 -0300

    finalizado

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -44,6 +44,8 @@ public partial class ListaProduto : ContentPage
 		{
 			string q = e.NewTextValue;
 
+			lst_produtos.IsRefreshing = true;
+
 			lista.Clear();
 
 			List<Produto> tmp = await App.Db.Search(q);
@@ -52,6 +54,10 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
 
     }
@@ -99,5 +105,22 @@ public partial class ListaProduto : ContentPage
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
 		
+    }
+
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+            List<Produto> tmp = await App.Db.GetAll();
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        } finally 
+		{
+			lst_produtos.IsRefreshing = false;
+		}
     }
 }
